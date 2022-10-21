8bef998442a17681ab06d1d6299ce763aad53f3b
Ron Almog <ron8132@gmail.com>
2022-10-21 11:39:05 -0400

    server side protected page

---
+++ pages/protected2.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useSession, getSession, signIn, signOut } from "next-auth/react";
+import type { Session } from "next-auth";
+import { GetServerSideProps } from "next";
+
+type Props = {};
+
+const Protected2 = (props: Props) => {
+  const { data: session, status } = useSession();
+
+  if (status === "authenticated") {
+    return (
+      <div>
+        <p>Welcome {session?.user?.name}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>You are not signed in</p>
+      </div>
+    );
+  }
+};
+
+export default Protected2;
+
+export const getServerSideProps: GetServerSideProps<{
+  session: Session | null;
+}> = async (context) => {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+};
